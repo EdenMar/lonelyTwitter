33f6de7b5278adb404a85568d190b92fbf1b5fa9
mraypold <mraypold@gmail.com>
2014-01-14 17:46:38 -0700

    Added lonelyTweetModel constructors

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -9,6 +9,23 @@ public class lonelyTweetModel {
 	private String text;
 	private Date timestamp;
 	
+	public lonelyTweetModel(String text, Date timestamp)
+	{
+
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+	
+	public lonelyTweetModel(String text)
+	{
+
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
+
+
 	public String getText()
 	{
 	
